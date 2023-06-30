1027b28efe85b80f1a8c1b5024f396e0c40e376c
geoffwithers <geoffwithers91@gmail.com>
2023-06-30 03:05:58 -0400

    finished code, still not working

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const filesystem = require('./node_modules/graceful-fs/graceful-fs')
 const inquirer = require("inquirer");
 const {Circle, Square, Triangle} = require("./shapes");
-// Imports the graceful-fs, inquirer, Circle, Square, and Triangle modules.
-// Defines a Svg class that has a constructor with three methods for rendering and setting the text and shape elements in the SVG string.
 
 class Svg{
     constructor(){
@@ -23,33 +21,31 @@ class Svg{
     
 }
 
-// Defines array of 'questions' using the 'inquirer' library with the following questions.
-// Each question is an object that specifies the properties of TEXT, TEXT COLOR, SHAPE COLOR, and Pixel Image.
 const questions = [
     {
         type: "input",
         name: "text",
-        message: "TEXT: Enter up to (3) Characters:",
+        message: "TEXT: Please enter up to 3 Characters:",
     },
     {
         type: "input",
         name: "text-color",
-        message: "TEXT COLOR: Enter a color keyword (OR a hexadecimal number):",
+        message: "TEXT COLOR: Please enter a color keyword (or a hexadecimal number):",
     },
     {
         type: "input",
         name: "shape",
-        message: "SHAPE COLOR: Enter a color keyword (OR a hexadecimal number):",
+        message: "SHAPE COLOR: Please enter a color keyword (or a hexadecimal number):",
     },
     {
         type: "list",
         name: "pixel-image",
-        message: "Choose which Pixel Image you would like?",
+        message: "Please choose a shape for your logo:",
         choices: ["Circle", "Square", "Triangle"],
     },
 ];
 
-// Function to write data to file
+// writes data to file
 function writeToFile(fileName, data) {
 	console.log("Writing [" + data + "] to file [" + fileName + "]")
     filesystem.writeFile(fileName, data, function (err) {
@@ -65,31 +61,31 @@ async function init() {
 	var svgString = "";
 	var svg_file = "logo.svg";
 
-    // Prompt the user for answers
+   
     const answers = await inquirer.prompt(questions);
 
-	//user text
+	
 	var user_text = "";
 	if (answers.text.length > 0 && answers.text.length < 4) {
-		// 1-3 chars, valid entry
+		
 		user_text = answers.text;
 	} else {
-		// 0 or 4+ chars, invalid entry
-		console.log("Invalid user text field detected! Please enter 1-3 Characters, no more and no less");
+		
+		console.log("Invalid user text field");
         return;
 	}
 	console.log("User text: [" + user_text + "]");
-	//user font color
+	
 	user_font_color = answers["text-color"];
 	console.log("User font color: [" + user_font_color + "]");
-	//user shape color
+	
 	user_shape_color = answers.shape;
 	console.log("User shape color: [" + user_shape_color + "]");
-	//user shape type
+	
 	user_shape_type = answers["pixel-image"];
 	console.log("User entered shape = [" + user_shape_type + "]");
 	
-	//user shape
+	
 	let user_shape;
 	if (user_shape_type === "Square" || user_shape_type === "square") {
 		user_shape = new Square();
@@ -108,15 +104,15 @@ async function init() {
 	}
 	user_shape.setColor(user_shape_color);
 
-	// Create a new Svg instance and add the shape and text elements to it
+	
 	var svg = new Svg();
 	svg.setTextElement(user_text, user_font_color);
 	svg.setShapeElement(user_shape);
 	svgString = svg.render();
 	
-	//Print shape to log
+	
 	console.log("Displaying shape:\n\n" + svgString);
-	//document.getElementById("svg_image").innerHTML = svgString;
+	
 
 	console.log("Shape generation complete!");
 	console.log("Writing shape to file...");
